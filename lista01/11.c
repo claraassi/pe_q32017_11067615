4e3c49b0e520c6fe6ad8ebc10e192dcba732dbf1
Clara Assi <clarinhassi@gmail.com>
2017-09-25 19:07:54 -0300

    exercicio 11 da lista01

---
+++ lista01/11.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int valor,valor_final = 0,opcao;
+
+    /* le valor e opcao */
+    printf("Entre com o valor:");
+    scanf("%d",&valor);
+    printf("Entre com a opcao (1 converte de decimal para binario e 2 de binario para decimal):");
+    scanf("%d",&opcao);
+    
+    /* converte o valor */
+    switch (opcao) {
+        case 1:
+            if ((valor % 2) == 0) {
+                while (valor >= 1) {
+                    valor_final = (valor_final + (valor % 2)) * 10;
+                    valor = valor / 2;
+                }
+                printf("O valor em binario e %d\n",valor_final);
+                
+            } else {
+                while (valor > 1) {
+                    valor_final = (valor_final + (valor % 2)) * 10;
+                    valor = valor / 2;
+                }
+                valor_final = valor_final + 1;
+                printf("O valor em binario e %d\n",valor_final);
+            }
+            
+            break;
+        case 2:
+            
+            break;
+        default:
+            printf("Opcao invalida\n");
+            break;
+    }
+}
